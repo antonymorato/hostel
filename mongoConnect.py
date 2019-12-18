5d73955d223ed66e024b2b27ce0d2522336de7c9
antony <antonymorato2@gmail.com>
2019-12-18 16:25:01 +0200

    autorize

--- mongoConnect.py
+++ mongoConnect.py
@@ -12,7 +12,7 @@ class mongoConnect(object):
 	def connect(ip,port):
 		mongoConnect.conn = MongoClient("mongodb://{}:{}".format(ip,port))
 		mongoConnect.db=mongoConnect.conn.hostel
-		mongoConnect.collection=mongodb.db.rooms
+		mongoConnect.collection=mongoConnect.db.rooms
 
 	@staticmethod
 	def updateRoom(room,pc=default,microwave=default,
@@ -22,9 +22,30 @@ class mongoConnect(object):
 		new={'$set':{"pc":pc,
 		'microwave':microwave,
 		'fridge':fridge,'kettle':kettle}}
-	# @staticmethod
-	# def getStApplInfo(stId,room):
-	# 	mongoConnect.collection.find_one({})
+	
+
+	@staticmethod
+	def autorize(username,password):
+		mongoConnect.collection=mongoConnect.db.users
+		if mongoConnect.collection.find_one({'username':username,'password':password}):
+			return True
+		else:
+			return False
+
+	@staticmethod
+	def deleteUser():
+		mongoConnect.collection=mongoConnect.db.users
+		mongoConnect.collection.delete_one({'username':username})
+
+	@staticmethod
+	def addUser(username,password):
+		mongoConnect.collection=mongoConnect.db.users
+		if mongoConnect.collection.find_one({'username':username})==None:
+			mongoConnect.collection.insert_one({'username':username,'password':password})
+			return True
+		else:
+			return False
+
 	@staticmethod
 	def getRoomInfo(num):
 		#returns dict
@@ -33,7 +54,7 @@ class mongoConnect(object):
 	def addStudent(stId,room,pc=default,fridge=default,
 		microwave=default,kettle=default):
 		#mongoConnect.collection=mongoConnect.db.rooms
-		if mongoConnect.collection.find_one({'room':room,'students.stId'})!=None:
+	#	if mongoConnect.collection.find_one({'room':room,'students.stId'})!=None:
 			
 		if len(mongoConnect.collection.find_one({'room':room})['students'])<mongoConnect.maxSettlers:
 			query={'room':room,'students.id':{'$ne':stId}}
@@ -49,3 +70,11 @@ class mongoConnect(object):
 		query={'room':room}
 		update={'$set':{'cleanRating':rating}}
 		mongoConnect.collection.update_one(query,update,upsert=True)
+
+
+
+mongoConnect.connect('localhost','27017')
+
+#r=mongoConnect.autorize('toha','123456789')
+r=mongoConnect.addUser('toha','456789')
+print(r)
